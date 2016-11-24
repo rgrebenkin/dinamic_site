bb91e1bc21168ef91dca980731b583d31be5d26c
rgrebenkin <rgrebenkin@mail.ru>
2016-11-24 21:13:47 +0300

    commit

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -14,24 +14,27 @@ class Question(models.Model):
   text = models.TextField()
   added_at = models.DateTimeField(blank = True, auto_now_add=True)
   rating = models.IntegerField(default = 0)
-  author = models.ForeignKey(User)
-  likes = models.ManyToManyField(User, through="Likes")
+  #author = models.ForeignKey(User)
+  author = models.CharField(max_length=255)
+  #likes = models.ManyToManyField(User, through="Likes")
+  likes = models.ManyToManyField(User)
   objects = QuestionManager()
 
   
-class Likes(models.Model):
-    question = models.ForeignKey(
-        Question,
-        related_name="like_question"
-    )
-    user = models.ForeignKey(
-        User,
-        related_name="like_user"
-    )  
+#class Likes(models.Model):
+#    question = models.ForeignKey(
+#        Question,
+#        related_name="like_question"
+#    )
+#    user = models.ForeignKey(
+#        User,
+#        related_name="like_user"
+#    )  
 
   
 class Answer(models.Model):
   text = models.TextField()
   added_at = models.DateTimeField(blank = True, auto_now_add=True)
   question = models.ForeignKey(Question)
-  author = models.ForeignKey(User)
+  #author = models.ForeignKey(User)
+  author = models.CharField(max_length=255)
