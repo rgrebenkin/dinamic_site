3c825b1702af753ecc6dddc120e0950def81eda8
rgrebenkin <rgrebenkin@mail.ru>
2016-11-24 20:30:45 +0300

    commit

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -9,20 +9,29 @@ class QuestionManager(models.Manager):
   def popolar(self):
     return self.orderby('-rating').all()
   
+class Likes(models.Model):
+    question = models.ForeignKey(
+        Question,
+        related_name="like_question"
+    )
+    user = models.ForeignKey(
+        User,
+        related_name="like_user"
+    )  
 
 class Question(models.Model):
   title = models.CharField(max_length=255)
   text = models.TextField()
-  added_at = models.DateTimeField()
-  rating = models.IntegerField()
+  added_at = models.DateTimeField(blank = True, auto_now_add=True)
+  rating = models.IntegerField(default = 0)
   author = models.ForeignKey(User)
-  likes = models.ManyToManyField(User)
+  likes = models.ManyToManyField(User, through="Likes")
   objects = QuestionManager()
 
 
   
 class Answer(models.Model):
   text = models.TextField()
-  added_at = models.DateTimeField()
+  added_at = models.DateTimeField(blank = True, auto_now_add=True)
   question = models.ForeignKey(Question)
   author = models.ForeignKey(User)
